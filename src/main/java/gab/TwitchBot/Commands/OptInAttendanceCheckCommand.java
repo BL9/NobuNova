cf1e69c3d1f34ff1bd6019233228346f80fb2fa1
Beau-AP <s113303@ap.be>
2022-12-22 01:38:28 +0100

    Removed TwitchBot argument from existing commands that don't use it.

--- src/main/java/gab/TwitchBot/Commands/OptInAttendanceCheckCommand.java
+++ src/main/java/gab/TwitchBot/Commands/OptInAttendanceCheckCommand.java
@@ -2,13 +2,12 @@ package gab.TwitchBot.Commands;
 
 import java.io.IOException;
 
-import gab.TwitchBot.TwitchBot;
 import gab.TwitchBot.Utils.CommandEvent;
 import gab.Utils.Profile;
 import gab.Utils.ProfileHelper;
 
 public class OptInAttendanceCheckCommand extends Command{
-    public OptInAttendanceCheckCommand(TwitchBot bot) {
+    public OptInAttendanceCheckCommand() {
         this.name = "optin";
     }
 
