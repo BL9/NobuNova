4783b5fe46b7c4e2e3def75226ec436b160e2cb4
Beau-AP <s113303@ap.be>
2022-12-24 01:52:49 +0100

    Added description field and more more used exceptions

--- src/main/java/gab/DiscordBot/Commands/Command.java
+++ src/main/java/gab/DiscordBot/Commands/Command.java
@@ -1,13 +1,25 @@
 package gab.DiscordBot.Commands;
 
+import java.io.IOException;
+
+import javax.naming.NameNotFoundException;
+
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public abstract class Command {
     protected String name;
+    protected String description;
     
     public boolean isThisCommand(String command) {
         return (name.compareTo(command) == 0);
-    }
+    }    
 
-    public abstract void execute(SlashCommandInteractionEvent event);
+    public abstract void execute(SlashCommandInteractionEvent event) throws IOException, NameNotFoundException, InterruptedException;
+
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
 }
